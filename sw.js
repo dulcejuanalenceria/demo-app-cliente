44d4cc5667588edee2a6c094a3c43ff5c515dbe2
dulcejuanalenceria <riojascesarv@gmail.com>
2026-09-20 15:33:34 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE='base-comercial-demo-v1';
+const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
+self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
+self.addEventListener('fetch',e=>{e.respondWith(fetch(e.request).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html'))));});
